23fb5a82e368e88023656c9d5a7620d832b3f6d8
mjay.francis <mjay.francis@596a7bff-4a59-d6eb-2e58-145ae5ad41d3>
2011-05-25 11:30:21 +0000

    Improve demonstration compressor/decompressor

--- trunk/jbzip2/src/demo/Decompress.java
+++ trunk/jbzip2/src/demo/Decompress.java
@@ -23,14 +23,18 @@
 package demo;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.itadaki.bzip2.BZip2InputStream;
 
 /**
- * 
+ * A BZip2 file decompressor. For demonstration purposes only
  */
 public class Decompress {
 
@@ -40,13 +44,33 @@ public class Decompress {
 	 */
 	public static void main (String[] args) throws IOException {
 
-		InputStream fileInputStream = new BufferedInputStream (new FileInputStream (args[0]));
+		if (args.length == 0) {
+			System.err.println ("Demonstration BZip2 decompressor\n\nUsage:\n  java demo.Decompress <filename>\n");
+			System.exit (1);
+		}
+
+		File inputFile = new File (args[0]);
+		if (!inputFile.exists() || !inputFile.canRead() || !args[0].endsWith(".bz2")) {
+			System.err.println ("Cannot read file " + inputFile.getPath());
+			System.exit (1);
+		}
+
+		File outputFile = new File (args[0].substring (0, args[0].length() - 4));
+		if (outputFile.exists()) {
+			System.err.println ("File " + outputFile.getPath() + " already exists");
+			System.exit (1);
+		}
+
+		InputStream fileInputStream = new BufferedInputStream (new FileInputStream (inputFile));
 		BZip2InputStream inputStream = new BZip2InputStream (fileInputStream, false);
+		OutputStream fileOutputStream = new BufferedOutputStream (new FileOutputStream (outputFile), 524288);
 
 		byte[] decoded = new byte [524288];
 		int bytesRead;
-		while ((bytesRead = inputStream.read (decoded)) != -1) System.out.write (decoded, 0, bytesRead) ;
-		System.out.flush();
+		while ((bytesRead = inputStream.read (decoded)) != -1) {
+			fileOutputStream.write (decoded, 0, bytesRead) ;	
+		}
+		fileOutputStream.close();
 
 	}
 
